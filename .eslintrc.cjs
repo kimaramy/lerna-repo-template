8ef3eb98bc04cad94c5592af694b28eb1806bce6
Haram Kim <fed.haram@gmail.com>
2022-10-15 14:34:00 +0900

    config: eslint & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,66 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended', // Add plugin:prettier/recommended as the last extension
+    // 'prettier/@typescript-eslint', // prettier/@typescript-eslint has been removed in eslint-config-prettier v8.0.0.
+  ],
+  plugins: [],
+  rules: {
+    'no-console': 'warn',
+  },
+  overrides: [
+    {
+      files: 'packages/**/*.{ts,jsx,tsx}',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['packages/**/tsconfig*.json'],
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:react/jsx-runtime', // If you are using the new JSX transform from React 17, extend react/jsx-runtime in your eslint config, Make sure it is added after plugin:react/recommended in the extends array.
+        'plugin:jsx-a11y/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:prettier/recommended',
+      ],
+      settings: {
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+        },
+        react: {
+          version: 'detect',
+        },
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {},
+    },
+  ],
+};
